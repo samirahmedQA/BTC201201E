6368e5dbab7eafb8bb605d57f0746174c1cdf825
LAPTOP-NOTQIRID\SAMIR AHMED <ahmedkane12@yahoo.com>
2021-04-08 13:47:32 -0400

    HELLO

---
+++ Maven2021201E/src/main/java/until/Source.java
@@ -0,0 +1,9 @@
+package until;
+
+public class Source {
+
+	
+	public void m1() {
+		System.out.println();
+	}
+}
---
+++ Maven2021201E/src/test/java/com/smokeTest.java
@@ -0,0 +1,10 @@
+package com;
+
+public class smokeTest {
+
+	
+	@Test
+	public void 
+	
+	
+}
